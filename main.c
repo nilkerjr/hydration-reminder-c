dc494c203db485087fe04edb241d779e54ea5fb0
Stermi <pnbao2310@gmail.com>
2025-10-08 10:43:55 +0700

    Initial commit: C hydration reminder program

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <unistd.h>  // for sleep()
+#include <stdlib.h>
+
+void displayProgress(int waterIntake, int dailyGoal) 
+{
+    int progressUnits = (waterIntake * 20) / dailyGoal; // 20-unit bar
+    int i = 0;
+    printf("[");
+    for (i = 0; i < progressUnits; i++) printf("#");
+    for (i = progressUnits; i < 20; i++) printf("-");
+    printf("] %d%%\n", (waterIntake * 100) / dailyGoal);
+}
+
+int main() {
+    int dailyGoal = 0;
+    int waterIntake = 0;
+    int amount = 0;
+
+    printf("💧 Welcome to Hydration Reminder (C version)!\n");
+    printf("Enter your daily water goal in ml: ");
+    scanf("%d", &dailyGoal);
+
+    while (waterIntake < dailyGoal) {
+        printf("\nEnter amount of water you drank (ml): ");
+        scanf("%d", &amount);
+
+        waterIntake += amount;
+        if (waterIntake > dailyGoal) waterIntake = dailyGoal;
+
+        displayProgress(waterIntake, dailyGoal);
+
+        if (waterIntake < dailyGoal) {
+            printf("Keep going! Next reminder in 30 minutes.\n");
+            sleep(1800);  // wait 30 minutes (1800 seconds)
+        } else {
+            printf("🎉 Congratulations! You reached your daily goal.\n");
+        }
+    }
+
+    return 0;
+}
